8c2c263cee6071ed6c67815ca2b411659a68c795
Yannik Brändle <yannik.braendle@t-online.de>
2021-07-08 15:56:08 +0200

    Fixed error through gitkraken

--- Dodge-Box/src/DodgeBox.java
+++ Dodge-Box/src/DodgeBox.java
@@ -130,13 +130,10 @@ public class DodgeBox extends Application {
 		gameObjects.addAll(enemies);
 		collisionList.addAll(enemies);
 
-<<<<<<< Updated upstream
-=======
 		ChaserEnemyXRec chaser = new ChaserEnemyXRec(550, 200, 50, 50, false, player);
 		gameObjects.add(chaser);
 		collisionList.add(chaser);
-		
->>>>>>> Stashed changes
+
 		player.setFill(Color.BLUE);
 		border.getChildren().addAll(collisionList);
 		primaryStage.setScene(mainScene);
